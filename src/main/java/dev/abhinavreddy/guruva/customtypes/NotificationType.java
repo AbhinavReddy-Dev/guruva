663abdc490b60474f4741388dafd581e5086625a
AbhinavReddy-Dev <abhinavreddy9681@gmail.com>
2023-07-07 03:02:03 -0400

    updated notification types

--- src/main/java/dev/abhinavreddy/guruva/customtypes/NotificationType.java
+++ src/main/java/dev/abhinavreddy/guruva/customtypes/NotificationType.java
@@ -1,20 +1,32 @@
 package dev.abhinavreddy.guruva.customtypes;
 
 public enum NotificationType {
-//    A request to be a mentor has been sent to you
-    MENTOR_REQUEST("MENTOR_REQUEST"),
-    MENTOR_REQUEST_ACCEPTED("MENTOR_REQUEST_ACCEPTED"),
-    MENTOR_REQUEST_REJECTED("MENTOR_REQUEST_REJECTED"),
-//    A request to be a mentee has been sent to you
-    MENTEE_REQUEST("MENTEE_REQUEST"),
-    MENTEE_REQUEST_ACCEPTED("MENTEE_REQUEST_ACCEPTED"),
-    MENTEE_REQUEST_REJECTED("MENTEE_REQUEST_REJECTED"),
-//    A message has been sent to you
-    MESSAGE("MESSAGE"),
-//    A session request has been sent to you
-    SESSION_REQUEST("SESSION_REQUEST"),
-    SESSION_REQUEST_ACCEPTED("SESSION_REQUEST_ACCEPTED"),
-    SESSION_REQUEST_REJECTED("SESSION_REQUEST_REJECTED");
+
+//    user sends a request to mentor to be a mentee
+    REQ_USER_TO_MENTOR("REQ_USER_TO_MENTOR"),
+    ACC_USER_TO_MENTOR("ACC_USER_TO_MENTOR"),
+    REJ_USER_TO_MENTOR("REJ_USER_TO_MENTOR"),
+//    user send a request to mentee to be a mentor
+    REQ_USER_TO_MENTEE("REQ_USER_TO_MENTEE"),
+    ACC_USER_TO_MENTEE("ACC_USER_TO_MENTEE"),
+    REJ_USER_TO_MENTEE("REJ_USER_TO_MENTEE"),
+//    mentor sends a request to mentee to be a mentor
+    REQ_MENTOR_TO_MENTEE("REQ_MENTOR_TO_MENTEE"),
+    ACC_MENTOR_TO_MENTEE("ACC_MENTOR_TO_MENTEE"),
+    REJ_MENTOR_TO_MENTEE("REJ_MENTOR_TO_MENTEE"),
+//    mentee send a request to mentor to be a mentee
+    REQ_MENTEE_TO_MENTOR("REQ_MENTEE_TO_MENTOR"),
+    ACC_MENTEE_TO_MENTOR("ACC_MENTEE_TO_MENTOR"),
+    REJ_MENTEE_TO_MENTOR("REJ_MENTEE_TO_MENTOR"),
+//    mentor or mentee sends a feedback to user
+    FEEDBACK_MENTOR_TO_USER("FEEDBACK_MENTOR_TO_USER"),
+    FEEDBACK_MENTEE_TO_USER("FEEDBACK_MENTEE_TO_USER"),
+//    user removes a mentor or mentee
+    REM_USER_A_MENTOR_NF_MENTEE("REM_USER_A_MENTOR_NF_MENTEE"),
+    REM_USER_A_MENTEE_NF_MENTEE("REM_USER_A_MENTEE_NF_MENTEE"),
+//    mentee is closed by mentee or after a given time
+    CLOSE_MENTEE_NF_MENTOR("CLOSE_MENTEE_NF_MENTOR"),
+    CLOSE_MENTEE_NF_USER("CLOSE_MENTEE_NF_USER");
     private final String notificationType;
     NotificationType(String notificationType) {
         this.notificationType = notificationType;
